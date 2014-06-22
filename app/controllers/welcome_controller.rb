60bb6ed65f74f24b164a07f766cabca248ac4e72
Tom <t.offringa@st.hanze.nl>
2014-06-22 22:34:58 +0200

    made URL on two lines for rubocop to stop whining about line too long

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,7 +9,8 @@ class WelcomeController < ApplicationController
   private
 
   def url
-    "http://api.hooikoortsalert.nl/?login=true&host=www.hooikoortsalert.nl&username=#{ENV["API_USER"]}&key=#{ENV["API_KEY"]}"
+    "http://api.hooikoortsalert.nl/?login=true&host=www.hooikoortsalert.nl" \
+    "&username=#{ENV["API_USER"]}&key=#{ENV["API_KEY"]}"
   end
 
   def data
