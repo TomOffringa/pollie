018a5e755794bf3300243545322c720de35736c9
Willian <willian@pythonheads.nl>
2014-06-22 21:20:17 +0200

    Wow, such railsy

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,26 @@
 require 'open-uri'
+
 class WelcomeController < ApplicationController
   def index
-    @data = Nokogiri::XML(open("http://api.hooikoortsalert.nl/?login=true&host=www.hooikoortsalert.nl&username=#{ENV["API_USER"]}&key=#{ENV["API_KEY"]}"))
-    @status = @data.xpath("//desc").text
-    @colorclass = @data.xpath("//txt").text
+    @status = status
+    @color_class = color_class
+  end
+
+  private
+
+  def url
+    "http://api.hooikoortsalert.nl/?login=true&host=www.hooikoortsalert.nl&username=#{ENV["API_USER"]}&key=#{ENV["API_KEY"]}"
+  end
+
+  def data
+    @data ||= Nokogiri::XML(open(url))
+  end
+
+  def status
+    data.xpath("//desc").text
+  end
+
+  def color_class
+    data.xpath("//txt").text
   end
 end
